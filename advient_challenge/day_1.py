461dbc0257976004c5dded0d37e8efda78cb9a58
larry123 <larioslarry8@gmail.com>
2024-12-01 22:29:01 -0600

    doing the first day of the challenge

--- advient_challenge/day_1.py
+++ advient_challenge/day_1.py
@@ -1,5 +1,4 @@
 import requests
-from bs4 import BeautifulSoup
 import pandas as pd
 import os
 from dotenv import load_dotenv
@@ -11,14 +10,65 @@ url = "https://adventofcode.com/2024/day/1/input"
 
 # Realizar solicitud HTTP
 
-
-cookies = {'session': '53616c7465645f5fc02ccbfd8050003a2754315bcc63506a413e7d72a66a45fdc180b5c16e573c1f2b5ce111dfdcbf989b60b3b4ffac1068b86f3e994fe02d2b'}
+session = os.getenv("SESSION")
+cookies = {'session': session}
 
 response = requests.get(url, cookies=cookies)
 
-if response.status_code == 200:
+#print(response.text)
+
+text_strip = response.text.strip().split("\n")
+
+valores = []
 
 
-    soup = BeautifulSoup(response.text, 'html.parser')
-    print(soup)
+i = 1
+left = []
+right = []
+for line in text_strip:
+    splitted = line.split()
+    left.append(int(splitted[0]))
+    right.append(int(splitted[1]))
+
+
+
+left.sort()
+right.sort()
+
+len(right)
+len(left)
+
+#def result_recursion(i=0, acc=0):
+
+    #if len(left) ==i:
+        #return acc
     
+    #result = abs(left[i]-right[i])
+
+
+    #return(result_recursion(i+1, acc+result))
+
+result = 0
+
+for i in range(len(left)):
+    result += abs(left[i]-right[i])
+
+print(result)
+
+
+
+resultado = 0
+for i in range(len(left)):
+    if left[i] not in right:
+        resultado += 0
+    j = 0
+    if left[i] in right:
+        for item in right:
+            if item == left[i]:
+                j+=1
+        resultado += left[i]*j
+
+
+print(resultado)
+
+
